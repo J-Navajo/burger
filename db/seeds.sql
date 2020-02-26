d52a1613aec849aaa6b49e40e0d3fd39df2e34ee
J-Navajo <jnnfr.stnly@gmail.com>
2020-02-25 23:14:21 -0700

    updating seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
-INSERT INTO burgers (name) VALUES ('cheeseburger');
-INSERT INTO burgers (name) VALUES ('bacon');
-INSERT INTO burgers (name, devoured) VALUES ('avocado', true);
-INSERT INTO burgers (name, devoured) VALUES ('turkey bacon', true);
-INSERT INTO burgers (name, devoured) VALUES ('mustard', true);
-INSERT INTO burgers (name) VALUES ('');
+INSERT INTO burgers (burger_name) VALUES ('cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('bacon');
+INSERT INTO burgers (burger_name, devoured) VALUES ('avocado', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('turkey bacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('mustard', true);
