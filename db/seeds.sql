eefd565c739e66297d1550f5e5b43b02c41f25f0
J-Navajo <jnnfr.stnly@gmail.com>
2020-02-25 21:57:38 -0700

    creating files to get started

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('cheeseburger');
+INSERT INTO burgers (name) VALUES ('bacon');
+INSERT INTO burgers (name, devoured) VALUES ('avocado', true);
+INSERT INTO burgers (name, devoured) VALUES ('turkey bacon', true);
+INSERT INTO burgers (name, devoured) VALUES ('mustard', true);
+INSERT INTO burgers (name) VALUES ('');
