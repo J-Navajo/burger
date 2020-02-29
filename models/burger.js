5c18cc3ac5b55b770d9dc83cfb328b9b719a1efb
J-Navajo <jnnfr.stnly@gmail.com>
2020-02-29 16:57:43 -0700

    adding functions to burger file

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,18 @@
 const orm = require("../config/orm.js");
 
 const burger = {
+    all: function(controllerCB) {
+        orm.selectAll("burgers", function(result) {
+            controllerCB(result)
+        })
+    },
 
+    insertOne: function(burger, controllerCB) {
+        orm.insertOne("burgers", ["burger", "devoured"],[burger, false], 
+        function(result) {
+            controllerCB(result)
+        })
+    }
 }
 
 module.exports = burger;
