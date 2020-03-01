9cbfec2fbb2fcc7c35f69e750cb4646bb17b6675
J-Navajo <jnnfr.stnly@gmail.com>
2020-02-29 17:17:45 -0700

    adding controller file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,7 +4,26 @@ const router = express.Router();
 
 const burger = require("..models/burger.js")
 
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        const burgerObject = { burger: data};
+        console.log(burgerObject);
+        res.render("index", burgerObject);
+    });
+});
 
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger", "devoured"
+    ],
+    [
+        req.body.burger,
+        req.body.devoured
+    ],
+    function(result) {
+        res.json({ id: result.insertId });
+    });
+});
 
 
 
