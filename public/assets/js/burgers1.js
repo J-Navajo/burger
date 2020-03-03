bc625f9e3f168cdd7d79bb42280558b349069da9
J-Navajo <jnnfr.stnly@gmail.com>
2020-03-02 22:10:33 -0700

    updating script file

--- public/assets/js/burgers1.js
+++ public/assets/js/burgers1.js
@@ -1,16 +1,42 @@
-$(".create-form").on("submit", function(event) {
-    event.preventDefault();
+$(function () {
+    $(".futureDevoured").on("click", function (event) {
+        event.preventDefault();
+        const id = $(this).data("id");
+        const newDevoured = $(this).data("newDevoured");
 
-    const newBurger = {
-        burger: $("burg").val()
-    };
+        const newDevouredState = {
+            devoured: newDevoured
+        };
 
-    $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-    })
-    .then(function() {
-        console.log("created a new burger");
-        location.reload();
-    })
-})
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            function () {
+                console.log("changed to Devoured", newDevoured);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+
+        const newBurger = {
+            burger: $("burg").val().trim(),
+            devoured: false
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        })
+            .then(function () {
+                console.log("created a new burger");
+                location.reload();
+            });
+    });
+});
