2e42c4757964fed00a664cac319c0fc4fc1ecc43
J-Navajo <jnnfr.stnly@gmail.com>
2020-03-02 22:40:44 -0700

    updated controller file with updateOne

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -29,7 +29,7 @@ router.put("/api/burgers:id", function (req, res) {
     const condition = "id = " + req.params.id;
     console.log("condition", condition);
 
-    burger.update({
+    burger.updateOne({
         devoured: req.body.devoured
     }, condition, function (result) {
         if (result.changedRows == 0) {
